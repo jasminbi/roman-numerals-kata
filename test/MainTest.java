ea339ddb844780648ca150e6439fc6a5e9d0f2f7
jasminbi <jasmin.bissig@gmail.com>
2018-04-23 14:52:52 +0200

    Insert loop for numbers divisible by 10

--- test/MainTest.java
+++ test/MainTest.java
@@ -15,7 +15,7 @@ public class MainTest {
     public void convert_2_to_II() {
         int input = 2;
         String roman = convertRomanNumerals(input);
-        assertEquals("II",roman);
+        assertEquals("II", roman);
     }
 
     @Test
@@ -32,20 +32,41 @@ public class MainTest {
         assertEquals("V", roman);
     }
 
+    @Test
+    public void convert_10_to_X() {
+        int input = 10;
+        String roman = convertRomanNumerals(input);
+        assertEquals("X", roman);
+    }
+
+    @Test
+    public void convert_20_to_XX() {
+        int input = 20;
+        String roman = convertRomanNumerals(input);
+        assertEquals("XX", roman);
+    }
+
     private String convertRomanNumerals(int input) {
 
         String result = "";
 
         if (input == 5) {
-            return "V";
+            result = "V";
+
+        } else if (input % 10 == 0) {
+            String romanX = "X";
+            for (int i = 0; i < input; i += 10) {
+                result += romanX;
+            }
+
+        } else {
+            String romanI = "I";
+            for (int i = 0; i < input; i++) {
+                result += romanI;
+            }
         }
 
-        String romanI = "I";
-        for (int i = 0; i < input; i++) {
-            result += romanI;
-        }
         return result;
-
 
 
     }
