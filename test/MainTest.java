449e8ccb7382ad12da386ff1e7178ebabb2ee800
jasminbi <jasmin.bissig@gmail.com>
2018-04-23 15:29:45 +0200

    Add logic for different roman letters

--- test/MainTest.java
+++ test/MainTest.java
@@ -1,3 +1,4 @@
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -46,16 +47,26 @@ public class MainTest {
         assertEquals("XX", roman);
     }
 
+    @Test
+    public void convert_11_to_XI() {
+        int input = 11;
+        String roman = convertRomanNumerals(input);
+        assertEquals("XI", roman);
+    }
+
     private String convertRomanNumerals(int input) {
 
         String result = "";
 
-        if (input % 10 == 0) {
-            result = composeRoman(input, result, "X", 10);
+        if (input >= 10) {
+            int wieOft10 = input / 10;
+            result += composeRoman(wieOft10, "X", 1);
+            int wieOft1 = input % 10;
+            result += composeRoman(wieOft1, "I", 1);
         } else if (input % 5 == 0) {
-            result = composeRoman(input, result, "V", 5);
+            result += composeRoman(input, "V", 5);
         } else {
-            result = composeRoman(input, result, "I", 1);
+            result += composeRoman(input, "I", 1);
         }
 
         return result;
@@ -63,11 +74,12 @@ public class MainTest {
 
     }
 
-    private String composeRoman(int input, String result, String romanLetter, int divisor) {
-        for (int i = 0; i < input; i += divisor) {
-            result += romanLetter;
+    private String composeRoman(int limit, String romanLetter, int divisor) {
+        String toAdd = "";
+        for (int i = 0; i < limit; i += divisor) {
+            toAdd += romanLetter;
         }
-        return result;
+        return toAdd;
     }
 
 }
